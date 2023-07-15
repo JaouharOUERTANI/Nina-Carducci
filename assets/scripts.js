49736d18fd0baac7dcdd65b6f460b4297bc4d708
jaouhar ouertani <jaouhar.ouertani@gmail.com>
2023-07-15 09:56:50 +0200

    Création fichier .htaccess et Readme

--- assets/scripts.js
+++ assets/scripts.js
@@ -15,19 +15,3 @@ $(document).ready(function() {
 });
 
 //Pour retirer le chevron de droite du carousel
-
-<script>
-    document.addEventListener('DOMContentLoaded', function () {
-        var carouselInner = document.getElementById('carousel-inner');
-        var prevButton = document.querySelector('.carousel-control-prev');
-
-        carouselInner.addEventListener('slid.bs.carousel', function () {
-            // Vérifier si la première photo est active
-            if (carouselInner.querySelector('.carousel-item:first-of-type').classList.contains('active')) {
-                prevButton.style.display = 'none'; // Masquer le bouton précédent
-            } else {
-                prevButton.style.display = 'block'; // Afficher le bouton précédent
-            }
-        });
-    });
-</script>
